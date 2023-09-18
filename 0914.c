91afda9b90dd4e90557f32c37ded0008803df7ac
SoWiEee <99129463+SoWiEee@users.noreply.github.com>
2023-09-18 11:30:32 +0800

    Create 0914.c

---
+++ 0914.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+// 指標Pointer
+
+int main() {
+  char voc[1000][10];
+  int count[1000];
+  while(EOF != scanf("%s",voc){
+    
+      
+      scanf("&s",&voc);
+      for(i=0; i<=10; i++){
+        if(){
+          count[]+=1;
+        }
+      }
+      printf("\n");
+    
+  }
+}
